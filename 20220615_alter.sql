50d66bde5aa52405553ddece13f2b18b84b9a9e5
Bonghui(Nora) <74698994+Nora1476@users.noreply.github.com>
2022-06-15 23:59:59 +0900

    Delete 20220615_alter.sql

--- 20220615_alter.sql
+++
@@ -1,9 +0,0 @@
-select * from tab;
-
-alter table dept add (reg_date date); --dept reg_date행 추가 
-alter table dept rename column reg_date to reg;--추가한 행 이름 변경
-alter table dept modify reg varchar2(20); --행 타입변경
-alter table dept drop column reg; --reg행 지우기
-alter table dept set unused(dno); --dno 행 숨기기
-alter table detp drop unused columns;  --숨긴테이블 지워버리기
-desc dept;--dept 테이블 보기
